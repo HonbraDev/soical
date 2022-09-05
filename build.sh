48f1bbac3011f999f046e066ffd5a577211742cd
Honbra <honbra@honbra.com>
2022-09-05 20:21:56 +0200

    Add a build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Adapted from https://github.com/matrix-org/dendrite/blob/main/build.sh (Apache 2.0)
+
+# Put installed packages into ./bin
+export GOBIN=$PWD/`dirname $0`/bin
+
+mkdir -p bin
+
+CGO_ENABLED=0 go build -trimpath -ldflags "$FLAGS" -v -o "bin/" ./cmd/...

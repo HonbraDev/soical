355fbebbd4aa595f3d2e64e5bdfae3c8325fb18f
Honbra <honbra@honbra.com>
2022-09-05 22:01:23 +0200

    Docker fix 4 (please work)

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 # Adapted from https://github.com/matrix-org/dendrite/blob/main/build.sh (Apache 2.0)
-
-# Put installed packages into ./bin
-export GOBIN=$PWD/`dirname $0`/bin
-
 mkdir -p bin
-
 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -v -o "bin/" ./cmd/...
